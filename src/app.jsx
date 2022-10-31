1385566139fd4d3eda4a7b0d1c5107674d55a903
jhlee <wisdom_13_@naver.com>
2022-10-31 13:14:50 +0900

    PostCSS

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import './app.css';
+import Button1 from './Button1';
+import Button2 from './Button2';
+
+function App() {
+  return (
+    <>
+      <Button1></Button1>
+      <Button2></Button2>
+    </>
+  );
+}
+
+export default App;
