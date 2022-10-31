39ddc6f35a596d1e9ef38c69705e82ffa4c0559d
jhlee <wisdom_13_@naver.com>
2022-10-31 14:04:27 +0900

    Tailwind

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TailwindComponent = () => {
+  return (
+    <div>
+      <h1 className='text-3xl'>안녕!</h1>
+    </div>
+  );
+};
+
+export default TailwindComponent;
--- src/app.jsx
+++ src/app.jsx
@@ -1,38 +1,21 @@
 import './app.css';
 import Button1 from './Button1';
 import Button2 from './Button2';
-import styled, { css } from 'styled-components'
-
-const Container = styled.div`
-  display: flex;
-`;
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #3c5b69;
-  color: #b9eaff;
-  margin: 0.1em;
-  padding: 0.25em 1em;
-  ${(props) =>
-    props.primary &&
-    css`
-      background: #009cd5;
-      color: white;
-    `}
-`;
+import StyledComponent from './StyledComponent';
+import TailwindComponent from './TailwindComponent';
 
 function App() {
   return (
     <>
-      <h1>PostCSS</h1>
+      <h1>[PostCSS]</h1>
       <Button1></Button1>
       <Button2></Button2>
-      <h1>Styled Components</h1>
-      <Container>
-        <Button>Normal Button</Button>
-        <Button primary>Primary Button</Button>
-      </Container>
+      <br /><br />
+      <h1>[Styled Components]</h1>
+      <StyledComponent></StyledComponent>
+      <br />
+      <h1>[Tailwind Component]</h1>
+      <TailwindComponent></TailwindComponent>
     </>
   );
 }
